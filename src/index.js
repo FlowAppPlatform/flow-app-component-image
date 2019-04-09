75fe2ee7bf1bfd499ff8a61f99716475c77c5799
Jerry Edebua <jeredebua@gmail.com>
2019-04-09 15:21:33 +0300

    sets image to fill container

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import AppComponent from 'flow-app-component';
 // Image Canvas Styles
 import './style.css';
 
+// Programmatically generated styles
+import {
+  alignContainer,
+  containerWidth,
+  displayType,
+  alignVertical
+} from './style';
+
 class ImageComponent extends AppComponent {
   constructor() {
     super();
     const newState = {
-      properties: [
-        {
+      properties: [{
           categoryName: 'General',
           categoryDescription: 'Basic settings for image.',
-          properties: [
-            {
+          properties: [{
               id: 'image',
               name: 'Image',
               type: 'image',
@@ -29,15 +35,105 @@ class ImageComponent extends AppComponent {
               data: 0,
               renderOnPropsPanel: false,
             },
+            {
+              id: 'align-container',
+              name: 'Align Container',
+              type: 'position',
+              options: ['left', 'center', 'right'],
+              data: null,
+            },
+            {
+              id: 'container-width',
+              name: 'Width',
+              type: 'dropdown',
+              options: {
+                options: [{
+                    label: '10%',
+                    value: 'ten'
+                  },
+                  {
+                    label: '20%',
+                    value: 'twenty'
+                  },
+                  {
+                    label: '30%',
+                    value: 'thirty'
+                  },
+                  {
+                    label: '40%',
+                    value: 'forty'
+                  },
+                  {
+                    label: '50%',
+                    value: 'fifty'
+                  },
+                  {
+                    label: '60%',
+                    value: 'sixty'
+                  },
+                  {
+                    label: '70%',
+                    value: 'seventy'
+                  },
+                  {
+                    label: '80%',
+                    value: 'eighty'
+                  },
+                  {
+                    label: '90%',
+                    value: 'ninety'
+                  },
+                  {
+                    label: '100%',
+                    value: 'full-page'
+                  }
+                ]
+              },
+              data: null,
+            },
+            {
+              id: 'vertical-align',
+              name: 'Vertical Align',
+              type: 'dropdown',
+              options: {
+                options: [{
+                    label: 'Top',
+                    value: 'top'
+                  },
+                  {
+                    label: 'Middle',
+                    value: 'middle'
+                  },
+                  {
+                    label: 'Bottom',
+                    value: 'bottom'
+                  },
+                ]
+              },
+              data: null
+            },
           ],
         },
         {
           categoryName: 'Events',
           categoryDescription: 'Events for the image',
-          properties: [
+          properties: [{
+              id: 'load',
+              name: 'Load Event',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
             {
-              id: 'event',
-              name: 'Events',
+              id: 'click',
+              name: 'Click Event',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+            {
+              id: 'hover',
+              name: 'Hover Event',
               type: 'graph',
               options: {},
               data: null,
@@ -56,33 +152,72 @@ class ImageComponent extends AppComponent {
     this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
   }
 
+  componentDidMount() {
+    this.triggerGraphEvent('load')
+  }
+
+  triggerGraphEvent = (eventId) => {
+    const graphId = this.getPropertyData(eventId);
+    if (typeof this.getElementProps().onEvent === 'function') {
+      this.getElementProps().onEvent(graphId);
+    }
+  }
+
   renderContent() {
     const elemProps = this.getElementProps();
+    const defaultWidth = {
+      width: '100%'
+    };
+    const defaultVerticalAlign = {
+      verticalAlign: 'top'
+    }
+    
+    // filter out unwanted props
     elemProps.style = Object.assign(this.getDefaultStyle() || {}, {
       backgroundImage: `url(${this.getPropertyData('image') ||
         '/assets/images/cloud.jpg'})`,
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
+      content: `url(${this.getPropertyData('image') ||
+      '/assets/images/cloud.jpg'})`,
+      width: '100%',
+      ...this.getPropertyData('vertical-align') &&
+      alignVertical(this.getPropertyData('vertical-align').value) || defaultVerticalAlign,
+      ...this.getPropertyData('container-width') &&
+      containerWidth(this.getPropertyData('container-width').value) || defaultWidth,
+      ...this.getPropertyData('align-container') &&
+      alignContainer(this.getPropertyData('align-container')),
     });
-      //Filter out unwanted props
-      const {
-          parentId,
-          componentType,
-          componentData,
-          isDragging,
-          canAcceptDrop,
-          hasChildren,
-          getComponent,
-          getComponentType,
-          getComponentPropertyData,
-          setPropertyData,
-          moveUI,
-          moveInto,
-          setHoverObject,
-          hoverObject,
-          ...props
-      } = elemProps;
-    return <div className="node image" {...props} />;
+    
+    const {
+      parentId,
+      componentType,
+      componentData,
+      isDragging,
+      canAcceptDrop,
+      hasChildren,
+      getComponent,
+      getComponentType,
+      getComponentPropertyData,
+      setPropertyData,
+      moveUI,
+      moveInto,
+      setHoverObject,
+      hoverObject,
+      ...props
+    } = elemProps;
+    return (
+      <div
+        className = "node image"
+        onMouseOver = {
+          () => this.triggerGraphEvent('hover')
+        }
+        onClick = {
+          () => this.triggerGraphEvent('click')
+        }
+        { ...props }
+      />
+    );
   }
 }
 
